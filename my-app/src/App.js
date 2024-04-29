2d2d03923bbc6cb782c711729ebad71afa450d22
Madhumidha08 <madhusakthees2001@gmail.com>
2024-04-29 23:22:00 +0530

    Test commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar/NavBar.jsx";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar />
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+                 >
+                 </a>
       </header>
     </div>
   );
