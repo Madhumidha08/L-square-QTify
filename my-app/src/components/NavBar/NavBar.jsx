a12170ca0901b3d5b2786c4d45fbee7285a86837
Madhumidha08 <madhusakthees2001@gmail.com>
2024-05-07 22:15:49 +0530

    Test commit

--- my-app/src/components/NavBar/NavBar.jsx
+++ my-app/src/components/NavBar/NavBar.jsx
@@ -5,17 +5,17 @@ import SearchBox from "../SearchBox/SearchBox";
 import FeedbackButton from "../FeedbackButton/FeedbackButton";
 import { Link } from "react-router-dom";
 
-function Navbar(){
-    const placeholder = "Search a album of your choice"
-    return(
+function Navbar() {
+    const placeholder = "Search an album of your choice"; // Corrected placeholder text
+    return (
         <nav className={styles.navbar}>
             <Link to="/">
-                <Logo/>
+                <Logo />
             </Link>
-            <SearchBox placeholder={placeholder}/>
-            <FeedbackButton/>
+            <SearchBox placeholder={placeholder} />
+            <FeedbackButton />
         </nav>
-    )
+    );
 }
 
 export default Navbar;
