1fb7dca67d7bd3482f4b04a2e86c27b7f65c1f60
Madhumidha08 <madhusakthees2001@gmail.com>
2024-05-08 23:53:35 +0530

    Test commit

---
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './Navbar.module.css';
+import Logo from "../Logo/Logo";
+import SearchBox from "../SearchBox/SearchBox";
+import FeedbackButton from "../FeedbackButton/FeedbackButton";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    const placeholder = "Search an album of your choice"; // Corrected placeholder text
+    return (
+        <nav className={styles.navbar}>
+            <Link to="/">
+                <Logo />
+            </Link>
+            <SearchBox placeholder={placeholder} />
+            <FeedbackButton />
+        </nav>
+    );
+}
+
+export default Navbar;
